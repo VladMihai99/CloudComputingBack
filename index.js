f028a0f30d5e3d029e9f3246cb154526b591b9b7
unknown <vlad6561@gmail.com>
2022-05-13 21:50:25 +0300

    First routes and a router

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+const messagesRouter = require('./router/messagesRouter');
 
 const app = express();
+
 app.use(cors());
 
-const port = process.env.port || 8080;
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use("/messages", messagesRouter);
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Cloud Computing app listening on port ${port}!`)
