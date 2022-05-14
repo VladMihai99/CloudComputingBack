c9f7c432f48ddb7af9cf945df336c67b26a07b74
unknown <vlad6561@gmail.com>
2022-05-14 16:07:06 +0300

    Mail implementation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const messagesRouter = require('./router/messagesRouter');
+const utilsRouter = require('./router/utilsRouter');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use("/messages", messagesRouter);
+app.use("/utils", utilsRouter);
 
 const port = process.env.PORT || 8080;
 
